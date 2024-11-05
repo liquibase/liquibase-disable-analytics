b2961b1dc0175faca3a17a63b7ac1a9439a4fb07
Steven Massaro <steven.massaro.web@gmail.com>
2024-11-05 12:19:31 -0700

    DAT-18938: remove hardcoded code after release (#6)

--- src/main/java/org/liquibase/analytics/DisablingAnalyticsConfigurationValueProvider.java
+++ src/main/java/org/liquibase/analytics/DisablingAnalyticsConfigurationValueProvider.java
@@ -1,6 +1,7 @@
 package org.liquibase.analytics;
 
 import liquibase.Scope;
+import liquibase.analytics.configuration.AnalyticsArgs;
 import liquibase.command.CommandScope;
 import liquibase.configuration.ConfigurationValueProvider;
 import liquibase.configuration.ProvidedValue;
@@ -16,7 +17,7 @@ public class DisablingAnalyticsConfigurationValueProvider implements Configurati
     public ProvidedValue getProvidedValue(String... keyAndAliases) {
         // We are purposefully hardcoding the string here instead of referencing the key directly so this extension
         // can be released prior to doing a Liquibase release.
-        if (keyAndAliases[0].equals("liquibase.analytics.enabled")) {
+        if (keyAndAliases[0].equals(AnalyticsArgs.ENABLED.getKey())) {
             String message = "Analytics is being forcibly disabled because the liquibase-disable-analytics extension is " +
                     "on the classpath. If this is not your intention, remove the extension. If you do intend to disable " +
                     "analytics, note that you can disable analytics with the --analytics-enabled=false argument, or associated " +
